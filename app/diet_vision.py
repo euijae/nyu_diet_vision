b69ce7e66dfd2d7aa46237cb76a1fddf673034e4
Euijae Kim <kimeuijae4@gmail.com>
2023-08-03 21:26:37 -0400

    Add files via upload

--- app/diet_vision.py
+++ app/diet_vision.py
@@ -1,19 +1,17 @@
 import calendar
 import cv2
 import datetime
-import glob
 import random
 import torch
 import os
 
 import supervision as sv
 import numpy as np
+import itertools as it
 
-from datasets import load_dataset
 from pathlib import Path
 from PIL import Image
 from segment_anything import sam_model_registry, SamAutomaticMaskGenerator
-from transformers import CLIPProcessor, CLIPModel
 
 
 class DietVision:
@@ -31,91 +29,89 @@ class DietVision:
 
         self.mask_generator = SamAutomaticMaskGenerator(sam)
         self._index_group_list = []
-        self._overlay_image_stack = []
-        self._maximum_image_stack_size = 50
-
-    def _cleanup_image_stack(self) -> None:
-        if len(self._overlay_image_stack) >= self._maximum_image_stack_size:
-            images = glob.glob(f'{self.image_directory}/overlay_image*.png')
-
-            for image in images:
-                os.system(f'rm {image}')
-
-            self._overlay_image_stack.clear()
 
     def upload_image(self, image_name):
-        # TODO this must be set dynamically
-        # image_name = 'image_original.jpg'
         self.image_directory = os.path.join(self.HOME, 'images')
         
         self.image_path = os.path.join(self.image_directory, image_name)
         self.image_brg = cv2.imread(self.image_path) # np.ndarray
         self.original_image = cv2.cvtColor(self.image_brg, cv2.COLOR_BGR2RGB) # np.ndarray
         
-        app_posix = Path(__file__).parent.parent.absolute() / "app"
-        app_home = str(app_posix.as_posix())
+        # app_posix = Path(__file__).parent.parent.absolute() / "app"
+        # app_home = str(app_posix.as_posix())
         # with open(f'{app_home}/sam_result.npy', 'rb') as f:
         #     self.sam_result = np.load(f, allow_pickle=True).copy()
-            
+
         self.sam_result = self.mask_generator.generate(self.original_image)
 
         self._init_diet_vision_dictionary()
-        self._init_mask_dictionary()
         self._generate_blank_transparent_image()
 
         return self.create_overlay_image()
 
     def _init_diet_vision_dictionary(self):
         """One time set up at image upload"""
-        self.diet_vision_dictionary = [{
+        self.raw_diet_vision_dictionary = [{
             'mask': mask['segmentation'],
             'area': mask['area'],
-            'bbox': mask['bbox'],
-            'combined_area': 0,
             'class': None,
-            'attached_to': -1,
-        } for mask in sorted(self.sam_result, key=lambda x: x['area'], reverse=True)]
+            'attached_to': idx,
+            'nonzero_at': set(np.rec.fromarrays(np.nonzero(mask['segmentation'])).tolist())
+        } for idx, mask in enumerate(sorted(self.sam_result, key=lambda x: x['area'], reverse=True))]
+        
+        self.height, self.width = self.raw_diet_vision_dictionary[0]['mask'].shape
 
-    def _init_mask_dictionary(self):
-        """One time set up at image upload"""
-        self.height, self.width = self.diet_vision_dictionary[0]['mask'].shape
-        self.mask_dictionary = np.full((self.height, self.width), -1)
+        mask_dictionary = np.full((self.height, self.width), -1)
 
-        for mi, item in enumerate(self.diet_vision_dictionary):
-            x, y, w, h = item['bbox']
-            item.update({'attached_to': mi})
-            
-            for row in range(y, y+h+1):
-                for col in range(x, x+w+1):
-                    if item['mask'][row][col]:
-                        if self.mask_dictionary[row][col] == -1:
-                            self.mask_dictionary[row][col] = mi
-                        else:
-                            item['attached_to'] = self.mask_dictionary[row][col]
+        for idx, curr_dvd in enumerate(self.raw_diet_vision_dictionary):
+            for pixel in curr_dvd['nonzero_at']:
+                if mask_dictionary[pixel] == -1:
+                    mask_dictionary[pixel] = idx
+                else:
+                    mask_dictionary[pixel] = min(idx, mask_dictionary[pixel])
+
+                curr_dvd['attached_to'] = mask_dictionary[pixel]
+
+        dict_mask_groups = {}
+
+        for idx, dvd in enumerate(self.raw_diet_vision_dictionary):
+            if dvd['attached_to'] in dict_mask_groups:
+                dict_mask_groups[dvd['attached_to']].append(idx)
+            else:
+                dict_mask_groups.update({dvd['attached_to']: [idx]})
+
+        self.diet_vision_dictionary = [{
+            'mask': self._combine_masks_init(v),
+            'class': None,
+            'attached_to': idx,
+            'nonzero_at': set(np.rec.fromarrays(np.nonzero(self._combine_masks_init(v))).tolist()),
+            'area': len(set(np.rec.fromarrays(np.nonzero(self._combine_masks_init(v))).tolist()))
+        } for idx, v in enumerate(dict_mask_groups.values())]
 
     def _generate_blank_transparent_image(self):
         """One time set up at image upload"""
         transparent_image_file_name = 'blank_transparent_image.png'
         self.blank_transparent_image_path = os.path.join(self.HOME, 'images', transparent_image_file_name)
 
-        if not os.path.exists(self.blank_transparent_image_path):
-            blank_white_image = 255 * np.ones((self.height, self.width, 3), dtype = np.uint8)
-            im_rgba = Image.fromarray(blank_white_image).convert('RGBA')
-            im_rgba_data = im_rgba.getdata()
-            im_rgba_new_data = []
+        if os.path.exists(self.blank_transparent_image_path):
+            os.system(f'rm {self.blank_transparent_image_path}')
 
-            for item in im_rgba_data:
-                if item[0] == 255 and item[1] == 255 and item[2] == 255:
-                    im_rgba_new_data.append((255, 255, 255, 0))
-                else:
-                    im_rgba_new_data.append(item)
+        blank_white_image = 255 * np.ones((self.height, self.width, 3), dtype = np.uint8)
+        im_rgba = Image.fromarray(blank_white_image).convert('RGBA')
+        im_rgba_data = im_rgba.getdata()
+        im_rgba_new_data = []
 
-            im_rgba.putdata(im_rgba_new_data)
-            im_rgba.save(self.blank_transparent_image_path, "PNG")
+        for item in im_rgba_data:
+            if item[0] == 255 and item[1] == 255 and item[2] == 255:
+                im_rgba_new_data.append((255, 255, 255, 0))
+            else:
+                im_rgba_new_data.append(item)
 
-    def create_overlay_image(self, indices: list = None) -> str:
+        im_rgba.putdata(im_rgba_new_data)
+        im_rgba.save(self.blank_transparent_image_path, "PNG")
+
+    def create_overlay_image(self, is_all: bool = True, indices: list = None) -> str:
         """Annotate mask and put that on blank image"""
-        is_all = False if indices else True
         transparent_image_test = cv2.imread(self.blank_transparent_image_path)
         transparent_image_test = transparent_image_test.copy()
         annotated_mask_test = self._generate_image_annotator() if is_all else self._spot_annotator_on_mask(indices)
@@ -155,7 +151,8 @@ class DietVision:
         for idx in lst:
             if idx in mask_index_set:
                 mask_index_list = self._find_attached_index_list(idx)
-                mask_index_set -= set(mask_index_list)
+                print(f'{idx} = {mask_index_list}')
+                # mask_index_set -= set(mask_index_list)
 
                 if overlay_image is None:
                     overlay_image = self._spot_annotator_on_mask(mask_index_list)
@@ -194,17 +191,28 @@ class DietVision:
             combined_mask = np.logical_or(combined_mask, curr_mask)
 
         return combined_mask
+    
+    def _combine_masks_init(self, index_list: list) -> np.ndarray:
+        """(utlity) Annotate mask and put that on blank image"""
+        i0 = index_list[0]
+        combined_mask = self.raw_diet_vision_dictionary[i0]['mask'].copy()
+
+        for idx in index_list[1:]:
+            curr_mask = self.raw_diet_vision_dictionary[idx]['mask'].copy()
+            combined_mask = np.logical_or(combined_mask, curr_mask)
+
+        return combined_mask
 
     def find_mask_index_list(self, bbox_plot: list, is_collect: bool = False) -> list:
         x1, y1, x2, y2 = bbox_plot
 
         index_set = set()
+        diet_vision_dictionary_size = len(self.diet_vision_dictionary)
 
-        for row in range(y1, y2+1):
-            for col in range(x1, x2+1):
-                curr_mask_index = self.mask_dictionary[row][col]
-                if curr_mask_index > -1:
-                    index_set.add(curr_mask_index)
+        for row, col in it.zip_longest(range(y1, y2+1), range(x1, x2+1)):
+            for idx in range(0, diet_vision_dictionary_size):
+                if (row, col) in self.diet_vision_dictionary[idx]['nonzero_at']:
+                    index_set.add(idx)
 
         if is_collect:
             lst = self._index_group_list.copy()
@@ -226,7 +234,7 @@ class DietVision:
         """(utility) retrieve food class and its area"""
         target_index = self.diet_vision_dictionary[selected_index]['attached_to']
 
-        index_set = set([target_index])
+        index_set = set()
 
         for mask_idx, segment_object in enumerate(self.diet_vision_dictionary):
             if segment_object['attached_to'] == target_index:
@@ -244,64 +252,12 @@ class DietVision:
         return area
     
     def update_food_class(self, updated_food_class: str) -> None:
-        parent_index = min(self._index_group_list)
-        for idx in self._index_group_list:
-            self.diet_vision_dictionary[idx].update({ 
-                'class': updated_food_class, 
-                'attached_to': parent_index 
-            })
-
         if self._index_group_list:
+            parent_index = min(self._index_group_list)
+            for idx in self._index_group_list:
+                self.diet_vision_dictionary[idx].update({ 
+                    'class': updated_food_class, 
+                    'attached_to': parent_index 
+                })
+
             self._index_group_list.clear()
-    
-    def expand_mask_index_list(self, index_list: list) -> list:
-        lst = index_list.copy()
-
-        for idx in index_list:
-            lst += self._find_attached_index_list(idx)
-
-        return sorted(list(set(lst)))
-    
-    @DeprecationWarning
-    def _init_image_classification(self):
-        imagenette = load_dataset('food101')
-        self.labels = imagenette['train'].features['label'].names
-
-        # generate sentences
-        self.clip_labels = [f"a photo of a {label}" for label in self.labels]
-        model_id = "openai/clip-vit-base-patch32"
-
-        self.processor = CLIPProcessor.from_pretrained(model_id)
-        self.model = CLIPModel.from_pretrained(model_id)
-
-    @DeprecationWarning
-    def _food_classification(self, image_ndarray: np.ndarray) -> any:
-        # create label tokens
-        label_tokens = self.processor(
-            text=self.clip_labels,
-            padding=True,
-            images=None,
-            return_tensors='pt'
-        ).to(self.DEVICE)
-
-        # encode tokens to sentence embeddings
-        label_emb = self.model.to(self.DEVICE).get_text_features(**label_tokens)
-
-        # detach from pytorch gradient computation
-        label_emb = label_emb.detach().cpu().numpy()
-
-        # normalization
-        label_emb = label_emb / np.linalg.norm(label_emb, axis=0)
-
-        image = self.processor(
-            text=None,
-            images=Image.fromarray(image_ndarray),
-            return_tensors='pt'
-        )['pixel_values'].to(self.DEVICE)
-
-        img_emb = self.model.get_image_features(image)
-        img_emb = img_emb.detach().cpu().numpy()
-        scores = np.dot(img_emb, label_emb.T)
-        pred = np.argmax(scores)
-
-        return self.labels[pred]
--- app/main.py
+++ app/main.py
@@ -39,7 +39,6 @@ app.add_middleware(
 
 app.dv_instance = None
 
-
 templates = Jinja2Templates(directory='templates')
 round_off = lambda x: map(lambda f : round(float(f)), x)
 
@@ -57,33 +56,50 @@ async def main(request: Request):
 
 @app.post('/segment/group')
 async def group_segments(bbox: BBox):
-    x1, y1, x2, y2 = list(round_off([bbox.x1, bbox.y1, bbox.x2, bbox.y2]))
-    indices = getInstance().find_mask_index_list(list(round_off([x1, y1, x2, y2])), True)
+    try:
+        bbox_plot = list(round_off([bbox.x1, bbox.y1, bbox.x2, bbox.y2]))
+        indices = getInstance().find_mask_index_list(bbox_plot, True)
 
-    overlay_image_path = getInstance().create_overlay_image(False, indices)
-    overlay_image_name = overlay_image_path.split('/')[-1]
+        if not indices:
+            return {'file_name': getInstance().blank_transparent_image_path}
 
-    return {'file_name': overlay_image_name}
+        overlay_image_path = getInstance().create_overlay_image(False, indices)
+        overlay_image_name = overlay_image_path.split('/')[-1]
+
+        return {'file_name': overlay_image_name}
+
+    except Exception as ex:
+        return {'error_message': ex}
 
 
 @app.post('/segment/data')
 async def get_object_data(bbox: BBox):
-    x1, y1, x2, y2 = list(round_off([bbox.x1, bbox.y1, bbox.x2, bbox.y2]))    
-    indices1 = getInstance().find_mask_index_list(list(round_off([x1, y1, x2, y2])))
-    indices2 = getInstance().expand_mask_index_list(indices1)
+    try:
+        bbox_plot = list(round_off([bbox.x1, bbox.y1, bbox.x2, bbox.y2])) 
+        indices = getInstance().find_mask_index_list(bbox_plot, False)
 
-    obj_class, area = getInstance().get_data_by_mask_index(mask_index=indices2[0])
+        if not indices:
+            return {"class": "n/a (The pixel doesn't belong to any objects)", 'volume': 0 }
+
+        obj_class, area = getInstance().get_data_by_mask_index(mask_index=indices[0])
+        
+        return { 'class': obj_class, 'volume': area }
     
-    return { 'class': obj_class, 'volume': area }
+    except Exception as ex:
+        return {'error_message': ex}
 
 
 @app.post('/classify/modify')
 async def update_food_class(fc: FoodClass):
-    getInstance().update_food_class(fc.food_class)
-    overlay_image_path = getInstance().create_overlay_image(True)
-    file_name = overlay_image_path.split('/')[-1]
+    try:
+        getInstance().update_food_class(fc.food_class)
+        overlay_image_path = getInstance().create_overlay_image(True)
+        file_name = overlay_image_path.split('/')[-1]
 
-    return { 'updated_food_class': fc.food_class, 'file_name': file_name }
+        return { 'updated_food_class': fc.food_class, 'file_name': file_name }
+
+    except Exception as ex:
+        return {'error_message': ex}
 
 
 @app.get('/clear')
@@ -94,10 +110,10 @@ async def clear_images(request: Request):
 
 @app.post('/upload/image')
 async def save_image(filename: str = Form(...), filedata: str = Form(...)):    
+    # Pass
     try:
         static_directory = getInstance().HOME
-        image_directory = os.path.join(static_directory, 'images')
-        file_location = os.path.join(image_directory, filename)
+        file_location = os.path.join(static_directory, 'images', filename)
 
         image_as_bytes = str.encode(filedata)
         img_recovered = base64.b64decode(image_as_bytes)
@@ -106,29 +122,8 @@ async def save_image(filename: str = Form(...), filedata: str = Form(...)):
             f.write(img_recovered)
             overlay_image_path = getInstance().upload_image(filename)
             overlay_image_name = overlay_image_path.split('/')[-1]
+
             return { "file_name": overlay_image_name }
-    except Exception:
-        return { "message": "There was an error uploading the file" }
 
-
-def get_nutrition_info(food_name):
-    food_name = food_name.replace('_', ' ')
-
-    #Make request to Nutritionix API
-    response = requests.get(
-        "https://trackapi.nutritionix.com/v2/search/instant",
-        params={"query": food_name},
-        headers={
-            "x-app-id": "a3663db3",
-            "x-app-key": "06eee2ab6e395eb02e4500d70f0fcdee"
-        }
-    )
-    #Parse response and return relevant information
-    data = response.json()
-    response = data["branded"][0]["photo"]["thumb"]
-    return {
-        "food_name": data["branded"][0]["food_name"],
-        "calories": data["branded"][0]["nf_calories"],
-        "serving_size": data["branded"][0]["serving_qty"],
-        "serving_unit": data["branded"][0]["serving_unit"]
-    }
+    except Exception as ex:
+        return {'error_message': ex}
